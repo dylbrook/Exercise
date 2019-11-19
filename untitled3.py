33f1007b121e5787230dc463309faf205a0b8537
Dylbrook <dyllanthiodoris@gmail.com>
2019-11-19 16:33:07 +0700

    This is a first add

---
+++ untitled3.py
@@ -0,0 +1,61 @@
+Message = "Dear Mom, Dad, and Brother, i would like to invite you to my house to have some dinner"
+print (Message)
+
+people = ["Mom", "Dad", "Brother"]
+print (people)
+
+print("Mom cant come")
+
+people[0] = 'Bob'
+print (people)
+
+Message_to_Dad = "Dear Dad, Mom couldnt come to dinner so i invited Bob"
+print(Message_to_Dad)
+
+Message_to_Brother = "Dear Brother, Mom couldnt come to dinner so i invited Bob"
+print (Message_to_Brother)
+
+Message_to_Bob = "Dear bob i would like to invite you to our dinner tonight"
+print(Message_to_Bob)
+
+statement = "I got informed that 3 other people will come to our dinner tonight"
+print (statement)
+
+people.insert(1, "Dave")
+people.insert(1, "Melissa")
+people.insert(1, "Brian")
+
+print (people)
+
+
+Message_to_Dad = "Dear Dad, Mom couldnt come to dinner so i invited Bob"
+print(Message_to_Dad)
+
+Message_to_Brother = "Dear Brother, Mom couldnt come to dinner so i invited Bob"
+print (Message_to_Brother)
+
+Message_to_Brian = "Dear bob i would like to invite you to our dinner tonight"
+print(Message_to_Bob)
+
+Message_to_Brian = "Dear Brian, thank you for your time to come to our dinner"
+print(Message_to_Brian)
+
+Message_to_Dave = "Dear Dave, thank you for your time to come to our dinner"
+print (Message_to_Dave)
+
+Message_to_Melissa = "Dear Melissa, thank you for your time to come to our dinner"
+print(Message_to_Melissa)
+
+places = ["London", "NewYork", "Tokyo", "Paris", "Bali"]
+print(places)
+print(sorted(places))
+print(places)
+print(sorted(places, reverse = True))
+print(places)
+places.sort (reverse = True)
+print(places)
+places.reverse
+print(sorted(places))
+print(places)
+places.sort(reverse = False)
+print(places)
